5cb6b55f2bcb6051e8e42ceeb85eab39933e9b80
vzdrizhni <vzdrizhni@gmail.com>
2020-08-17 15:10:32 +0500

    Change color of buttons

--- src/components/button.component.jsx
+++ src/components/button.component.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Button = (props) => (
-                <button className={`${props.wide}-button`}>{props.button}</button>
+        <button className={`${props.wide}-button-${props.color}`}>{props.button}</button>
 )
 
 Button.propTypes = {
-        button: PropTypes.string.isRequired
+        button: PropTypes.string.isRequired,
+        wide: PropTypes.bool,
+        color: PropTypes.string
 }
 
 export default Button
